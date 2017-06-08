1fc9e39823b028f7b840600d3663db8adc64f5f3
Uilian Souza <uilian.souza@softdesign.com.br>
2017-06-08 15:16:42 -0300

    Preparando para utilizar nova fonte de dados, devido a mudanca no site dos correios.

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,23 @@
 $(document).ready( function(){
-	window.addEventListener("load", initialize);
+   window.addEventListener("load", initialize);
+   $('#table_id').DataTable({
+      language: {
+         "url": "datatables/i18n/pt_br.json"
+      },
+      paging: true,
+      pageLength: 10,
+      lengthChange: false,
+      searching: false,
+      ordering: true,
+      info: true,
+      autoWidth: true
+    });   
 });
 
 function initialize(){
-	var idList = localStorage["objIdentifiers"];
-	var url = "http://websro.correios.com.br/sro_bin/txect01$.Inexistente?P_LINGUA=001&P_TIPO=002&P_COD_LIS=";
+   var idList = localStorage["objIdentifiers"];
+   var url = "http://websro.correios.com.br/sro_bin/txect01$.Inexistente?P_LINGUA=001&P_TIPO=002&P_COD_LIS=";
    //var url = "http://websro.correios.com.br/sro_bin/sroii_xml.eventos?Usuario=ECT&Senha=SRO&Tipo=L&Resultado=U&Objetos="
-	url = url + idList;
-   
-	console.log(url);
-	$("#iContent").attr('src', url);			
+   url = url + idList;
+   console.log(url);   
 }
